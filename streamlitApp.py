11a1a2c4d9c12f3ca15513643fe2c732426416cf
Kevin Yonamine <kyonamine@yext.com>
2024-04-22 11:39:17 -0400

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -241,7 +241,7 @@ if __name__ == "__main__":
 
         with st.form("Form"):
             frame = uploadFile()
-            frame.dropna(inplace = True)
+            # frame.dropna(inplace = True)
 
             filterData = ''
             daterange = ''
