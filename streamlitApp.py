d1f744f855f02ea5afd3181ae0a40e117babe320
Kevin Yonamine <kyonamine@yext.com>
2024-04-22 09:43:23 -0400

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -119,13 +119,13 @@ def deletePost(accountId, postIdList, externalId, heads):
     df = pd.DataFrame(columns = ['Google Location ID', 'localPostId', 'API Response Code'])
     # fullApi = baseApi + str(externalId) + '/localPosts/' + str(postId)
     os.write(1,  f"{postIdList}\n".encode())
-    for postId in range(len(postIdList)):
-        call = baseApi + str(externalId) + '/localPosts/' + str(postId)
-        r_info = requests.delete(call, headers = heads)
-        response = r_info.status_code
-        df.loc[i] = [externalId, postId, response]
-        os.write(1,  f"{call}\n".encode())
-    os.write(1,  f"{df}\n".encode())
+    # for postId in range(len(postIdList)):
+        # call = baseApi + str(externalId) + '/localPosts/' + str(postId)
+        # r_info = requests.delete(call, headers = heads)
+        # response = r_info.status_code
+        # df.loc[i] = [externalId, postId, response]
+        # os.write(1,  f"{call}\n".encode())
+    # os.write(1,  f"{df}\n".encode())
     return response
 
 def filterByKeyText(df, filterData, apiFieldKey):
