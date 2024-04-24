22318cfd4cf12f4bd14268808297ee0eccf6bbe3
Kevin Yonamine <kyonamine@yext.com>
2024-04-24 14:53:39 -0400

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -341,10 +341,10 @@ if __name__ == "__main__":
                     
             elif field == 'Social Posts':
                 for i in listGoogleIds:
-                    os.write(1,  f"{i}\n".encode())
                     if googleAccountNum == '':
                         exitApp(3)
                     else:
+                        os.write(1,  f"{i}\n".encode())
                         response = loopThroughIds(googleAccountNum, 'Social Posts', i, headers)
                         if not isinstance(response, pd.DataFrame):
                             locationLog = pd.DataFrame({'Google Location ID': [i], 'localPostId': [response], 'API Response Code': [200]})
