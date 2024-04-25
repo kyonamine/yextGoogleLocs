f8a0665473bdb6413af471bdbdbad7169a48feae
Kevin Yonamine <kyonamine@yext.com>
2024-04-25 10:42:28 -0400

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -87,7 +87,7 @@ def uploadFile():
                 dataframe['Yext ID'] = pd.to_numeric(dataframe['Yext ID'], errors = 'coerce')
                 dataframe['Google ID'] = pd.to_numeric(dataframe['Google ID'], errors = 'coerce')
                 dataframe = dataframe.dropna()
-
+                dataframe = dataframe.astype(str)
                 st.write(dataframe)
         return dataframe
     
