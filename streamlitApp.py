2f45b548fcc7b63d4fee60261111e749ae868536
Kevin Yonamine <kyonamine@yext.com>
2024-06-10 12:36:15 -0400

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -146,6 +146,7 @@ def loopThroughIds(accountId, endpoint, id, headers):
 def getMoreHoursCall(externalId, headers):
     fullApi = f'https://mybusinessbusinessinformation.googleapis.com/v1/locations/{str(externalId)}'
     r_info = requests.get(fullApi, headers = headers)
+    os.write(1,  f"Calling {fullApi}\n".encode())
     responseCode = r_info.status_code
     if responseCode != 200:
         if responseCode == 404:
