0a5623a548e8c4e2a621407052d573223f0373fb
Kevin Yonamine <kyonamine@yext.com>
2024-08-14 10:11:08 -0400

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -132,7 +132,9 @@ def loopThroughIds(accountId, endpoint, id, headers):
         response  = placeActionGetCall(id, headers)
     elif endpoint == 'Social Posts': # this isn't catching the 401 auth token errors. Place action works because it returns the code, but social post GET is returning a dataframe--- they might be getting caught now, not sure
         response = localPostGetCall(accountId, id, headers)
-    elif endpoint == 'All FAQs' or endpoint == 'Dupe FAQs':
+    elif endpoint == 'All FAQs':
+        response = getQuestions(id, headers)
+    elif endpoint == 'Dupe FAQs':
         response = getQuestions(id, headers)
     elif endpoint == 'Photos':
         response = getPhotosCall(accountId, id, headers)
