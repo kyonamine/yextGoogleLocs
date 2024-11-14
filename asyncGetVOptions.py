338998844f0ca16bcc2ed6f300f0367d0cb27e01
Kevin Yonamine <kyonamine@yext.com>
2024-11-14 16:06:30 -0500

    Update asyncGetVOptions.py

--- asyncGetVOptions.py
+++ asyncGetVOptions.py
@@ -2,17 +2,17 @@ import aiohttp
 import asyncio
 import pandas as pd
 
-async def getVOptions(externalId, headers):
+async def getVOption(externalId, headers):
     baseApi = f'https://mybusinessverifications.googleapis.com/v1/locations/{externalId}:fetchVerificationOptions'
     async with aiohttp.ClientSession() as session:
         async with session.get(baseApi, headers=headers) as r_info:
             responseCode = r_info.status
             if responseCode != 200:
                 if responseCode == 404:
-                    return 'Could not find location ' + str(externalId)
+                    return pd.DataFrame([{'verificationMethod': 0, 'phoneNumber': 0, 'error': 'Could not find location ' + str(externalId)}])
                 elif responseCode == 401:
-                    return 'Need authorization token for ' + str(externalId) + '!'
-                return 'Failed for ' + str(externalId)
+                    return pd.DataFrame([{'verificationMethod': 0, 'phoneNumber': 0, 'error': 'Need authorization token for ' + str(externalId) + '!'}])
+                return pd.DataFrame([{'verificationMethod': 0, 'phoneNumber': 0, 'error': 'Failed for ' + str(externalId)}])
             
             response = await r_info.json()
             try:
@@ -28,3 +28,8 @@ async def getVOptions(externalId, headers):
             except KeyError: 
                 df = pd.DataFrame([{'verificationMethod': 0, 'phoneNumber': 0, 'error': 'No verification options for ' + str(externalId)}])
             return df
+
+async def getVOptions(externalIds, headers):
+    tasks = [getVOption(externalId, headers) for externalId in externalIds]
+    results = await asyncio.gather(*tasks)
+    return pd.concat(results, ignore_index=True)
