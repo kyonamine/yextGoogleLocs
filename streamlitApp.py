bfe30a847c86a7675cea6714ddcaedc8f27c3d88
Wright Frost <wfrost@yext.com>
2025-03-06 17:11:17 -0500

    using session data for password properly

--- streamlitApp.py
+++ streamlitApp.py
@@ -25,6 +25,8 @@ st.set_page_config(page_title="Google Locations")
 
 if 'password_correct' not in st.session_state:
     st.session_state.password_correct = False  # Initialize to False
+if 'pw' not in st.session_state:  # Initialize 'pw' as well
+    st.session_state.pw = ''
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
@@ -35,29 +37,24 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["pw"] == st.secrets["pw"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["pw"]  # don't store password
+        if st.session_state.pw == st.secrets["pw"]:
+            st.session_state.password_correct = True
+            # del st.session_state["pw"]  # don't store password
         else:
-            st.session_state["password_correct"] = False
+            st.session_state.password_correct = False
 
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change = password_entered, key = "pw"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change = password_entered, key = "pw"
-        )
-        if "pw" in st.session_state and not st.session_state["password_correct"]:
-            st.error("😕 Password incorrect")
-        return False
-    else:
+    if st.session_state.password_correct:
         # Password correct.
         return True
+    else:
+        # Password not correct, show error.
+        if st.session_state.pw and not st.session_state.password_correct: # Check if a password HAS been entered
+          st.error("😕 Password incorrect")
+        return False
+    
+
+st.text_input("Password", type="password", on_change=password_entered, key="pw")
+
 
 def uploadFile():
     exampleSheet = 'https://docs.google.com/spreadsheets/d/18tJfjrlZFd3qQT5ZTnz3eIw5v6KSILTBN-Ol9G7sFLo/edit#gid=0'
